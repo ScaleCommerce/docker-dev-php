21015b8bcf362feb6720e5074e8c731ef847666c
Thomas Lohner <tl@scale.sc>
2019-08-18 16:06:15 +0200

    build PHP 5.6.40

--- build-php-versions.sh
+++ build-php-versions.sh
@@ -5,7 +5,7 @@ set -e
 
 for php in 5.6 7.0 7.1 7.2 7.3
   do 
-    sed -i -E "s/ PHP=[[:digit:]]\.[[:digit:]] / PHP=$php /" Dockerfile
+    sed -i '' -E "s/ PHP=[[:digit:]]\.[[:digit:]] / PHP=$php /" Dockerfile
     docker build -t scalecommerce-dev-php:local .
     local_version=$(cat version)
     php_version=$(docker run --rm scalecommerce-dev-php:local php -r "echo PHP_MAJOR_VERSION, '.', PHP_MINOR_VERSION, '.', PHP_RELEASE_VERSION;")
