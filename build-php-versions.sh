e70fbfa906a5bd7c6ec6fd16c970e57331e5f387
Thomas Lohner <tl@scale.sc>
2019-08-18 15:57:09 +0200

    build PHP 5.6.40

--- build-php-versions.sh
+++ build-php-versions.sh
@@ -9,7 +9,7 @@ for php in 5.6 7.0 7.1 7.2 7.3
     docker build -t scalecommerce-dev-php:local .
     local_version=$(cat version)
     php_version=$(docker run --rm scalecommerce-dev-php:local php -r "echo PHP_MAJOR_VERSION, '.', PHP_MINOR_VERSION, '.', PHP_RELEASE_VERSION;")
-    git commit -am "build PHP $php"
+    git commit -am "build PHP $php_version"
     git tag "$php_version-v$local_version"
     git push "$php_version-v$local_version"
   done
